956a6a406d8988231ac3f5338e3e289ea41d6e4e
alonsosanchezd <alonsosanchezd@yahoo.com>
2014-07-09 13:08:09 +0200

    Removed minder handler

--- src/com/copcar/android/Hmonitor.java
+++ src/com/copcar/android/Hmonitor.java
@@ -11,7 +11,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -25,33 +24,11 @@ public class Hmonitor extends CordovaPlugin {
 	public static final String ACTION_CHECK_CONNECTION_NUUBO = "checkConnection";
 
 	public static final String LOG_TAG = "hmonitor";
-	/**
-	 * public static final String TYPE_NORMAL = "normal"; public static final
-	 * String TYPE_ALERT = "alert"; public static final String TYPE_AMBULANCE =
-	 * "ambulance"; public static final String TYPE_AMBULANCE_FIELD =
-	 * "ambulance"; public static final String TYPE_CRITICAL = "critical";
-	 * public static final String TYPE_FIELD = "type"; public static final
-	 * String BPM_FIELD = "bpm"; public static final String LEVEL_FIELD =
-	 * "level"; public static final String RECORDS_FIELD = "records"; public
-	 * static final String CTE_LATITUDE_PARAM="latitude"; public static final
-	 * String CTE_LONGITUDE_PARAM="longitude"; public static final String
-	 * CTE_ACCURACY_PARAM="accuracy"; public static final String
-	 * CTE_MENSAJE_FIELD="mensaje"; public static final String
-	 * CTE_DEVICE_ID="deviceId"; public static final int CTE_TONE_DURATION=1000;
-	 * public static final String CTE_PLAINT_TEXT_MIME="text/plain"; private
-	 * static final Gson GSON_UTIL = new Gson(); private ToneGenerator
-	 * toneGenerator; private Vibrator vibrator; private static final String
-	 * CTE_URL =
-	 * "http://192.168.0.11/copcarws/rest/records/record/%s/%.8f/%.8f/%.8f/%d";
-	 * private final static AsyncHttpClient ASYNC_CLIENT= new
-	 * AsyncHttpClient(8080); private static ECGDevice nuuboDevice = null;
-	 * private static LogOnDisk log; private static TinyDB db;
-	 **/
-
+	
 	private static final String CTE_TYPE = "type";
 	private static final String CTE_STATUS = "status";
 	private static final String CTE_TYPE_INIT = "init";
-	private static final String CTE_TYPE_CONNECTION = "connection";
+	//private static final String CTE_TYPE_CONNECTION = "connection";
 	private static final String CTE_TYPE_BT = "bt";
 	private CallbackContext connectionCallbackContext;
 
@@ -61,10 +38,10 @@ public class Hmonitor extends CordovaPlugin {
 	// private LocationManager locationManager;
 	private BroadcastReceiver connectionReceiver;
 	private BroadcastReceiver btReceiver;
-	private BroadcastReceiver deviceReceiver;
+	
 	private boolean connectionRegistered = false;
 	private boolean btRegistered = false;
-	private boolean deviceRegistered = false;
+	
 	private ConnectivityManager sockMan;
 
 	@Override
@@ -81,8 +58,6 @@ public class Hmonitor extends CordovaPlugin {
 		addConnectionReceiver();
 		Log.i(LOG_TAG, "adding b");
 		addBTReceiver();
-		Log.i(LOG_TAG, "adding device reader");
-		addDeviceReceiver();
 	}
 
 	private void addBTReceiver() {
@@ -130,50 +105,6 @@ public class Hmonitor extends CordovaPlugin {
 		}
 	}
 
-	private void addDeviceReceiver() {
-		IntentFilter intentFilter = new IntentFilter();
-		intentFilter.addAction(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
-		intentFilter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
-		if (this.deviceReceiver == null) {
-			this.deviceReceiver = new BroadcastReceiver() {
-				@Override
-				public void onReceive(Context context, Intent intent) {
-
-					String action = intent.getAction();
-
-					// When discovery finds a device
-					BluetoothDevice device = intent
-							.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-					if (BluetoothDevice.ACTION_BOND_STATE_CHANGED.equals(action)) {
-						// Get the BluetoothDevice object from the Intent
-						if (device.getBondState()==BluetoothDevice.BOND_BONDED){
-							Log.i(LOG_TAG,
-									"bonded to Device with id:"
-											+ device.getName());	
-						}
-						else{
-							Log.i(LOG_TAG,
-									"not bonded to Device with id:"
-											+ device.getName());
-						}
-						
-					}
-					else{
-						if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
-							// Get the BluetoothDevice object from the Intent
-
-							Log.i(LOG_TAG,
-									"connected to Device with id:"
-											+ device.getName());
-						}
-					}
-				}
-			};
-			cordova.getActivity().registerReceiver(this.deviceReceiver,
-					intentFilter);
-			this.deviceRegistered = true;
-		}
-	}
 
 	private void addConnectionReceiver() {
 		IntentFilter intentFilter = new IntentFilter();
@@ -224,17 +155,7 @@ public class Hmonitor extends CordovaPlugin {
 						"Error unregistering btReceiver: " + e.getMessage(), e);
 			}
 		}
-		if (this.deviceReceiver != null && this.deviceRegistered) {
-			try {
-				this.cordova.getActivity().unregisterReceiver(
-						this.deviceReceiver);
-				this.deviceRegistered = false;
-			} catch (Exception e) {
-				Log.e(LOG_TAG,
-						"Error unregistering deviceReceiver: " + e.getMessage(),
-						e);
-			}
-		}
+		
 	}
 
 	@Override
